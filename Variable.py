d96a307a1c21dc607ecc5c83c26b6643628f03d5
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-06-29 23:21:12 -0700

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,39 @@
+# A Variable is a container for data value
+# Like other programming language You can define the variable of python also in same way
+# A varible name can be anything
+
+#Code Start From Here...
+#Declaring Variable in python
+
+print('Arfin Shariar')
+print(20)
+
+#We can store this 2 data in 2 varible. Let's Do it...
+
+print('After Declaring Variable')
+print('------------------------')
+x = 'Arfin Shariar' #Here x is a variable
+y = 2 # Y is also a variable
+city = 'Dhaka'
+number = 30
+
+print(x)
+print(y)
+print(city)
+print(number)
+
+"""
+Output:
+_____________
+
+Arfin Shariar
+20
+
+After Declaring Variable
+------------------------
+Arfin Shariar
+2
+Dhaka
+30
+
+"""
