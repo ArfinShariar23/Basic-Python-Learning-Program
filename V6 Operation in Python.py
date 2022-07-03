24e05730f3852d7c5405d9c11298b8bcbc047c8d
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-07-03 10:58:15 -0700

    Add files via upload

---
+++ V6 Operation in Python.py
@@ -0,0 +1,30 @@
+
+#Arethmetic Operation in Python
+x = 10
+y = 5
+
+#Assignment Operation
+print(x+y) #addition
+print(x-y) #Substraction
+print(x*y) #Multiplication
+print(x/y) #division
+print(x%y) #modulas
+print(x**y) #exponantial
+
+#This all are called arithmetic operation
+
+#Relational Operator
+if x == y: # == is a relational operator
+    print("They are Equal")
+
+if x>y: # > is a relational operator
+    print("x is greater then y")
+
+if x<y: # < is a relational operator
+    print("x is less then y")
+
+if x<=y: # <= is a relational operator
+    print("x is equal or less then y")
+
+if x>=y: # >= is a relational operator
+    print("x is equal or greater then y")
