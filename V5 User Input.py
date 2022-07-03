a13deca40294986466bf8a47bd9c14a6a3e5f1b9
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-07-03 10:22:34 -0700

    Add files via upload

---
+++ V5 User Input.py
@@ -0,0 +1,16 @@
+
+#Take 2 number as input and find their sum
+x = int(input("Enter A Number: ")) #input taking
+y = int(input("Enter Second Number: ")) #input taking
+
+z = x+y  #Arithmetic Operation
+
+print("The Sum of x + y = ",z)
+
+#Take a String type name from user and print it
+nameL = input("Enter Your Last Name: ")
+nameF = input("Enter Your First Name: ")
+
+name = nameL+nameF
+
+print("CUSTOMER NAME: ",name)
