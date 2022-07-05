220990d740c50cc53028f9d030fd0053c22c9522
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-07-05 08:58:29 -0700

    Add files via upload

---
+++ V13 Dictionary in Python 2.py
@@ -0,0 +1,10 @@
+#Use of some useful function in Dictionaries
+
+#Create a dictionary here...
+dic1 = {1:"Java",2:"Python",3:"Ruby",4:"Pascel"}
+print(dic1)
+
+#use of some function...
+print(dic1.get(2)) #Output will be pytohn. because get() function give us the index value as output
+print(dic1.keys()) #it will print all the keys name in dictionary 1.
+print(dic1.items()) #it will print all the items name in dictionary 1.
