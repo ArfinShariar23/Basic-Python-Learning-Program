d54a8117269887f68ad191def1b606f44aa6cde8
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-07-05 10:00:41 -0700

    Add files via upload

---
+++ V17 Basic Problem 2.py
@@ -0,0 +1,11 @@
+# Find negative and positive number
+
+user_input = int(input("Enter any number: "))
+
+if user_input>=0:
+    print("This is a Positive number!!!")
+elif user_input<-1:
+    print("This is a Negative number!!!")
+
+else:
+    print("Invalid input!!!!")
