3049cc76ee257417d0106d55a8dabbbc49456cab
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-07-14 00:59:32 -0700

    Add files via upload

---
+++ V21 Ternary Operator.py
@@ -0,0 +1,6 @@
+#Ternary Operator is a process of giving conditions and printing output in same line
+#Let's see a example
+
+num = int(input("Enter any number: "))
+result = "Number is Even" if num%2==0 else "Number is Odd"
+print(result)
