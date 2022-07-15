d19947cc93af157c916772558a7cee6a6e594bfe
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-07-15 22:32:12 +0600

    Add files via upload

---
+++ V23 For Loops.py
@@ -0,0 +1,12 @@
+
+#Loop is used for comprehensive use of Program
+#Let's Create a Loop
+fruits = ['Apple','Orange','Cherry','Mango','Berry']
+for x in fruits:
+	print(x)
+	#This command will iterate fruit list from the first to end.
+print()
+#we can also looping through a String type value
+name = 'Shariar'
+for y in name:
+	print(y)
