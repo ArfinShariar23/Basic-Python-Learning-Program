45f960d887fce1e36f6bafea35e902b1d4d001ab
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-07-15 22:45:51 +0600

    Add files via upload

---
+++ V24 For Loop.py
@@ -0,0 +1,6 @@
+#Using Of Break Statement in Loops
+fruit = ['Apple','Mango','Cherry','Orange','Papaya','Berry',"Malta"]
+for x in fruit:
+	print(x)
+	if x == 'Cherry':
+		break
---
+++ V25 For Loop.py
@@ -0,0 +1,6 @@
+intput1 = int(input("Enter First Value: "))
+input2 = int(input("Enter Second Value: "))
+for x in range(intput1,input2):
+	print(x)
+
+print("Finish")
