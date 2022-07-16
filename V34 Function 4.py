fb2c72a023988d0e3b55912bb9d73b8a0e9c50db
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-07-16 23:25:59 +0600

    Update V34 Function 4.py

--- V34 Function 4.py
+++ V34 Function 4.py
@@ -1,10 +1,12 @@
-
+#If We add a '*' sign infront agrs then it will take unlimited input as parameter and take them as a tuple item. 
+#Then we can easily itterate them using for loop.
+#Creating a Function which name is Calculator
 def calculator(*args):
 	print(type(args))
 	temp = 0
-	for number in args:
-		temp = temp+number 
-	return temp 
+	for number in args: #using for loop to iterate our tuple item/value
+		temp = temp+number #addition process
+	return temp #return our function
 
-temp = calculator(1,2,22,12,17,21,98)
-print(temp)
+temp = calculator(1,2,22,12,17,21,98) #adding value in the temp
+print(temp) #printing temp
