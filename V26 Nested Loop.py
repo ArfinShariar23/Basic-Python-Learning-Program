c1a0e2556bfffe87dcbf25fe3b93bb88f6b87c95
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-07-16 11:34:50 +0600

    Add files via upload

---
+++ V26 Nested Loop.py
@@ -0,0 +1,6 @@
+#Nested Loop is use for use a second loop inside the first loop
+rgb = ["Green","Blue","Red"]
+fruit = ["Apple","Mango","Cherry"]
+for x in fruit:
+	for y in rgb:
+		print(x,y)
---
+++ V27 Hundred Time Sum Problem.py
@@ -0,0 +1,9 @@
+#Find the Sum of 1+2+3+.....+100
+
+n=1
+temp = 0
+for n in range(n,101):
+	temp=temp+n
+	n = n+1
+
+print(temp)
