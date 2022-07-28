0d361bdf4bc85030884e115ef1841e854dfd5d43
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-07-29 00:09:20 +0600

    Add files via upload

---
+++ Farenheit Converter.py
@@ -0,0 +1,7 @@
+
+#Temperature Converter in Python (Celcius to Farenheight)
+celcius = int(input("Enter Temperature in Calcius = "))
+
+far = (9*celcius)/5+32
+
+print("The Temperature in Farenheight = ",far)
