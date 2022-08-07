4a53e6650d6779f2d5eea0d3d6e0679966545720
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-08-07 23:01:28 +0600

    Add files via upload

---
+++ Faulty_Calculator.py
@@ -0,0 +1,25 @@
+# Project - Faulty Calculator
+print("Welcome to Casio Calculator")
+print("---------------------------")
+print("For Addition press - 1")
+print("For Substraction press - 2")
+print("For Multiplication press - 3")
+print("For Division press - 4")
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+user_input = int(input("Your Choice..."))
+if user_input == 1:
+    print("Sum of Two number = 55")
+
+elif user_input == 2:
+    print("Substraction of Two number = 23")
+
+elif user_input == 3:
+    print("Multiplication of Two number = 444")
+
+elif user_input == 4:
+    print("Division of Two number = 20")
+
+else:
+    print("Invalid Input")
+
