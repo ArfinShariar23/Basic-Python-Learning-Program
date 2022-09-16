e21e5bb08855bc59279caa522151356538c2e6b7
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-09-16 23:01:37 +0600

    Create time module.py

---
+++ time module.py
@@ -0,0 +1,25 @@
+import time
+
+# Local time check
+localtime = time.asctime(time.localtime(time.time()))
+print(localtime)
+
+print()
+
+
+
+# Checking Runtime of While Loop
+initial = time.time()
+k = 0
+while(k<46):
+	print(k,"This While Loop")
+	k+=1
+print("While Loop Run time: ",time.time()-initial)
+
+print()
+
+# Checking Runtime of forloop
+initial2 = time.time()
+for i in range(45):
+	print(i,"This is For Loop")
+print("For Loop Running time: ",time.time()-initial2)
