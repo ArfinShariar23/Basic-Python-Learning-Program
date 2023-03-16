56daa2b013cab9cff5adbe171eedcc3979b993a3
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2023-03-16 22:47:57 +0600

    Create Sum of List Number.py

---
+++ Function/Sum of List Number.py
@@ -0,0 +1,8 @@
+def sum_of_list(numbers):
+    total = 0
+    for num in numbers:
+        total = total+num
+    print("Total Sum of all numbers in List = ",total)
+
+my_number = [1,2,3,4,5]
+sum_of_list(my_number)
